12d47ecefcc1030e0fc63c86a7a0374fd2cbc3cc
luisalejandrojaramillo <luis.jaramillo-r@mail.escuelaing.edu.co>
2020-04-23 09:53:16 -0500

    Conexion al socket

--- server/index.js
+++ server/index.js
@@ -3,8 +3,16 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+//Usar un middleware de Node js
+app.use(express.static('client'));
+
 app.get('/hola-mundo',function (req,res) {
     res.status(200).send('Hola mundo desde una ruta');
+});
+
+//Permite lanzar eventos, abrimos una coneccion al Socket
+io.on('connection',function (socket) {
+    console.log("El nodo con IP:" + socket.handshake.address+" se ha conectado...");
 });
 
 server.listen(6677,function () {
